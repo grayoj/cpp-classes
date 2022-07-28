a9cebf56b94352a41c74726d00668ef171c9544e
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-07-28 09:54:40 +0100

    commenting in cpp programs.

---
+++ part_one/comments.cpp
@@ -0,0 +1,15 @@
+//You make comments using two forward slashes.
+
+//Include Input/Output Stream Standard Library.
+#include <iostream>
+//Use scope identifiers for standard library.
+using namespace std;
+
+//initialise main program
+//int main. Return an integer at the end of the main program.
+int main(){
+    // prints hello world to the terminal.
+    cout << "hi omar";
+    // returns an integer of 0; ending the program,
+    return 0;
+}
