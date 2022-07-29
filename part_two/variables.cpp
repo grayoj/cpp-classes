93cfb76728992b3d4c76c6acd8e57f6c219d8803
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-07-29 13:05:59 +0100

    Include: variables in C++

---
+++ part_two/variables.cpp
@@ -0,0 +1,19 @@
+// working with variables
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // declaring variables:
+    int x, y;
+    int result;
+    // process:
+    x = 8;
+    y = 2;
+    x = x + 1;
+    answer = x - y;
+    // print out the answer:
+    cout << answer;
+    // terminate the program:
+    return 0;
+}
