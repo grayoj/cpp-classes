72ef9aaffea8d71db314ec7a5f3b28ea8f78dfca
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-07-29 13:32:03 +0100

    introducing strings in cpp code.

---
+++ part_one/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// Strings in C++
+int main () {
+ string examplestring;
+ examplestring = "This is a string content";
+ cout << examplestring << endl;
+ examplestring = "This is another string content";
+ cout << examplestring << endl;
+ return 0;
+} 
